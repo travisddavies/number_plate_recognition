d7ed2354c0d977eb03b9135933366d5e7b768289
travisddavies <travisddavies@gmail.com>
2023-11-22 00:23:01 +1100

    removing unwanted chars in number plate

--- engine/recognition.py
+++ engine/recognition.py
@@ -24,7 +24,8 @@ class NumberPlateRecogniser:
         # OCR model for number plate text extraction.
         self._recogniser = PaddleOCR(use_angle_cls=True, lang='en',
                                      detect=True, rec=True,
-                                     rec_model_dir='./engine/best_weights/paddlepaddle')
+                                     rec_model_dir='./engine/best_weights/paddlepaddle',
+                                     show_log=False)
 
     # Detects the number plate in an image.
     def detect(self, image):
@@ -96,10 +97,23 @@ class NumberPlateRecogniser:
                 if self._is_confident_prediction(ordered_results):
                     for label_subtext in ordered_results:
                         label += label_subtext[1][0]
-
+                if '-' in label:
+                    print(label)
+                label = label.replace('-', '')
+                label = label.replace('.', '')
+                label = label.replace(',', '')
+                if '-' in label:
+                    print('after:', label)
                 labels.append(label)
 
         return labels
+
+    def remove_symbols(label):
+        possible_symbols = ['.', ',', '%', '$', '&', '-', '_', '+', '*', '@',
+                            '~', '#', '`', '~']
+        for symbol in possible_symbols:
+            label = label.replace(symbol, '')
+        return label
 
     def _is_confident_prediction(self, ordered_results):
         """
