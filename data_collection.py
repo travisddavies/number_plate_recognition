48611e4bcd729b3d7e6b4ad5ee0ff3c45481d9f2
travisddavies <travisddavies@gmail.com>
2023-12-03 21:22:33 +0800

    added more helper functions

--- data_collection.py
+++ data_collection.py
@@ -3,24 +3,26 @@ import time
 from metrics import get_iou
 
 
-def collect_data(frame, labels, present_number_plates, total_data):
+def collect_data(frame, labels, bboxes, total_data, times_on_screen):
     encoded_frame = base64.b64encode(frame)
     encoded_frame_string = encoded_frame.decode('utf-8')
 
-    for label in labels:
+    for label, bbox in zip(labels, bboxes):
         index = find_overlapping_bbox(label, bbox)
         if index >= 0:
+            update_time_on_screen(label, times_on_screen)
             update_labels(label, total_data, index, times_on_screen)
-        if label not in present_number_plates and label:
-            # data to send to db
+        else:
             data = {'numberplate': label,
                     'datetime': time.time(),
-                    'screenshot': encoded_frame_string}
-
+                    'screenshot': encoded_frame_string,
+                    'bbox': bbox}
             total_data.append(data)
-        present_number_plates.add(label)
+            index = len(total_data) - 1
 
-    return present_number_plates, total_data
+        update_bbox(total_data, index, bbox)
+
+    return total_data, times_on_screen
 
 
 def find_overlapping_bbox(label, bbox, total_data):
@@ -37,27 +39,29 @@ def find_overlapping_bbox(label, bbox, total_data):
 
 
 def update_labels(candidate_label, total_data, overlap_index, times_on_screen):
-    overlap = total_data[overlap_index]
-    overlapping_label = overlap['label']
     candidate_screen_time = get_time_on_screen(
         candidate_label, times_on_screen)
     overlap_screen_time = get_time_on_screen(
-        overlapping_label, times_on_screen)
+        total_data[overlap_index]['label'], times_on_screen)
 
     if candidate_screen_time > overlap_screen_time:
         total_data[overlap_index]['label'] = candidate_label
 
 
 def get_time_on_screen(label, times_on_screen):
-    if label not in times_on_screen:
-        start_time = time.time()
-        times_on_screen[label]['start_time'] = start_time
-        times_on_screen[label]['end_time'] = start_time
-        return 0
-
     start_time = times_on_screen[label]['start_time']
     end_time = times_on_screen[label]['end_time']
     time_on_screen = end_time - start_time
-    times_on_screen[label]['end_time'] = time.time()
 
     return time_on_screen
+
+
+def update_time_on_screen(label, times_on_screen):
+    if label not in times_on_screen:
+        times_on_screen[label]['start_time'] = time.time()
+
+    times_on_screen[label]['end_time'] = time.time()
+
+
+def update_bbox(total_data, index, bbox):
+    total_data[index]['bbox'] = bbox
