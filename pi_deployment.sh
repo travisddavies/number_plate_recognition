ab68d09997f38a44c6f402236d5edcd5bd8e1e3c
travisddavies <travisddavies@gmail.com>
2024-02-15 14:19:03 +0800

    remove pi_deployment.sh

--- pi_deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -r venv/number_plate
-python -m --system-site-packages venv venv/number_plate
-source venv/number_plate/bin/activate
-pip install --upgrade setuptools
-sudo apt-get install mupdf mupdf-tools libcap-dev python3-pyqt5 python3-opengl fonts-wqy-zenhei
-pip install -r pi_requirements.txt --upgrade
